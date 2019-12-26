dbf95fc5c9fd10bb741f7ab12c60b38702c99f28
vitorkaio <vitorcaiodepaula@gmail.com>
2019-12-26 16:32:35 -0300

    init push

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <Home />
+  </v-app>
+</template>
+
+<script>
+import Home from '@/pages/Home/Home';
+
+export default {
+  name: 'App',
+
+  components: {
+    Home
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/styles/GlobalStyle.js
@@ -0,0 +1,24 @@
+import { injectGlobal } from 'vue-styled-components';
+
+export default injectGlobal`
+	@import url('https://fonts.googleapis.com/css?family=Roboto:400,500&display=swap');
+  @import url('https://fonts.googleapis.com/css?family=Mansalva&display=swap');
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  
+  html, body, #root {
+    height: 100%;
+  }
+  body {
+    font: 14px 'Roboto', sans-serif;
+    background-color: red;
+    -webkit-font-smoonthing: antialiased !important;
+  }
+  ul {
+    list-style: none;
+  }
+`;
