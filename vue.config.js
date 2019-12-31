edb1521f84e358ccfea434fe30fb0e00673243be
vitorkaio <vitorcaiodepaula@gmail.com>
2019-12-30 23:28:01 -0300

    update manifest

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,25 @@ module.exports = {
     "vuetify"
   ],
   pwa: {
-    themeColor: "#303030",
-    name: "TESTE"
-  } 
+    name: 'My App',
+    themeColor: '#303030',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    
+    // configure the manifest options
+    manifestOptions: {
+      display: 'landscape',
+      background_color: '#42B883'
+      // ...other Manifest options...
+    },
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
+  }
 }
