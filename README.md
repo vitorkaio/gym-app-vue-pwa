0ada1399d9c95bedf2a0e8aca990a50b83b6b240
vitorkaio <vitorcaiodepaula@gmail.com>
2020-01-30 16:52:38 -0300

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# gym-app-vue-pwa
-pwa for gym app with vue
+# App - Ficha Academia
+
+## Tecnologias
+* Vue
+* Vuetify
+* Pwa
+* Node
+
+
+## Tela de Login
+<img src="imgs/login.png" width="36%" height="36%">
+
+<br />
+
+## Home
+<img src="imgs/home.png" width="36%" height="36%">
+
+<br />
+
+## Exercícios
+<img src="imgs/training.png" width="36%" height="36%">
+
+<br />
+
+## Finalizar treino
+<img src="imgs/training_end.png" width="36%" height="36%">
+
+<br />
+
+## Nova ordernação
+<img src="imgs/home2.png" width="36%" height="36%">
+
+<br />
+
+## Dados Pessoais
+<img src="imgs/personal.png" width="36%" height="36%">
+
+<br />
+
+## Mudar senha
+<img src="imgs/change_pass.png" width="36%" height="36%">
+
+<br />
+
+## Logout
+<img src="imgs/logout.png" width="36%" height="36%">
+
+<br />
+
+
--- src/services/apiGym/ApiGym.js
+++ src/services/apiGym/ApiGym.js
@@ -1,12 +1,19 @@
 import axios from 'axios';
 import Crypt from '../Crypt';
 
+const config = {
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    "Content-type": "application/json"
+  }
+}
+
 // const URL = 'http://192.168.1.112:3333';
 const URL = 'https://intense-hamlet-46083.herokuapp.com';
 
 export const getUser = async (id) => {
   try {
-    const user = await axios.get(`${URL}/users/${id}`);
+    const user = await axios.get(`${URL}/users/${id}`, config);
     if (user.data.code === 200) {
       return user.data.data;
     } else {
@@ -21,7 +28,7 @@ export const getUser = async (id) => {
 // caso contrário retorna null.
 export const login = async ({ username, password }) => {
   try {
-    const id = await axios.post(`${URL}/auth/login`, { username, password })
+    const id = await axios.post(`${URL}/auth/login`, { username, password }, config)
     if (id.data.code === 200) {
       const data = {
         id: id.data.data,
@@ -41,7 +48,7 @@ export const login = async ({ username, password }) => {
 // Edita a senha do usuário.
 export const editPassword = async (id, password) => {
   try {
-    const res = await axios.patch(`${URL}/users/${id}`, { password });
+    const res = await axios.patch(`${URL}/users/${id}`, { password }, config);
     if (res.data.code === 200) {
       return true;
     }
